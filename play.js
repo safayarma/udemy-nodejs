2960b43297e00dbda283ab7161009238fe2bfafb
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2022-05-18 13:51:01 -0400

    - refresher of core js syntax

---
+++ play.js
@@ -0,0 +1,16 @@
+var name = 'Don';
+var age = 29;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobbies) {
+	return (
+		'Name is ' + 
+		userName + 
+		', age is ' + 
+		userAge + 
+		', and the user has hobbies: ' +
+		userHasHobbies
+	);
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
