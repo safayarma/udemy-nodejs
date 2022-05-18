299dc6d041a2e1d7ea9f3958800ca7732050fe7a
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2022-05-18 15:34:08 -0400

    - arrays, objects, and reference types

--- play.js
+++ play.js
@@ -48,9 +48,10 @@ const hobbies = ['Sports', 'Cooking'];
 //for (let hobby of hobbies){
 //	console.log(hobby)
 //}
-console.log(hobbies.map(hobby => 'Hobby: hobby'));
+//console.log(hobbies.map(hobby => 'Hobby: hobby'));
+//console.log(hobbies);
+hobbies.push('Programming');
 console.log(hobbies);
-
 
 
 
