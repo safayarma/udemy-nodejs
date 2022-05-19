99dfb9a280b83ae1f80e70b2c63e446ec084365e
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2022-05-19 10:20:06 -0400

    - used requests

--- nodejs-first-server/app.js
+++ nodejs-first-server/app.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-	console.log(req);
+	console.log(req.url, req.method, req.headers);
+	//process.exit();
+	// CTRL + C in terminal will also quit running node.js server
 });
 
 server.listen(3000);
