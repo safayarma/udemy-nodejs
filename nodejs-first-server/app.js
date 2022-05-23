f7fab3438f19bab447169d2febff54ac6bca5243
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2022-05-23 11:16:47 -0400

    - added more middleware

--- nodejs-first-server/app.js
+++ nodejs-first-server/app.js
@@ -4,8 +4,14 @@ const express = require('express');
 
 const app = express();
 
-app.use(() => {
+app.use((req, res, next) => {
 	console.log('In the middleware!');
+	next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use(() => {
+	console.log('In another middleware!')
+	// ...
 });
 
 const server = http.createServer(app);
