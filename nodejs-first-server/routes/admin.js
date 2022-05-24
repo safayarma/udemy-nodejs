71430b2fcdad16e234a3749c0183d084265d9ac7
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2022-05-24 17:11:28 -0400

    - used helper function for navigation

--- nodejs-first-server/routes/admin.js
+++ nodejs-first-server/routes/admin.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 // /admin/add-product => GET
@@ -12,7 +14,7 @@ const router = express.Router();
 // });
 
 router.get('/add-product', (req, res, next) => {
-	res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+	res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
