5800b0ab707a138caf93e382bdadde090c65a255
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2022-05-29 19:55:24 -0400

    - share data across requests and users

--- nodejs-first-server/routes/admin.js
+++ nodejs-first-server/routes/admin.js
@@ -6,12 +6,7 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
-// /admin/add-product => GET
-// router.get('/add-product', (req, res, next) => {
-// 	res.send(
-// 		'<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-// 	);
-// });
+const products = [];
 
 router.get('/add-product', (req, res, next) => {
 	res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
@@ -19,8 +14,9 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-	console.log(req.body);
+	products.push({ title: req.body.title })
 	res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
