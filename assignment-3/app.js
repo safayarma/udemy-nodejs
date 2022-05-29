0f92e12843e37212c70527a5a00b0d4f64c63331
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2022-05-29 19:36:56 -0400

    - updated html and css

---
+++ assignment-3/app.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const express = require('express');
+
+const mainRoutes = require('./routes/index');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000);
