32d1a42548a68ddda63274a8f30225554ffca2d1
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2022-05-29 20:25:22 -0400

    - installed pug, handlebars, and ejs  - implemented pug

--- nodejs-first-server/app.js
+++ nodejs-first-server/app.js
@@ -7,6 +7,9 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 // Parses Incoming Requests
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
