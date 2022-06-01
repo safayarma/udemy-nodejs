24191aaf6ea7933f2619119502940c042e013d81
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2022-06-01 11:34:40 -0400

    - finishing pug template

--- nodejs-first-server/app.js
+++ nodejs-first-server/app.js
@@ -1,16 +1,16 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-// Parses Incoming Requests
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -18,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).render('404');
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
--- nodejs-first-server/routes/shop.js
+++ nodejs-first-server/routes/shop.js
@@ -8,8 +8,8 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	const products = adminData.products;
-	res.render('shop', {prods: products, docTitle: "Shop"});
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
