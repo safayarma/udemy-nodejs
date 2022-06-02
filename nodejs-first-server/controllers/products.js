64c1b4d4f7a4c3c9012bcb6381ecfe7c6d7c529a
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2022-06-02 19:40:20 -0400

    - finished adding controllers

--- nodejs-first-server/controllers/products.js
+++ nodejs-first-server/controllers/products.js
@@ -24,4 +24,8 @@ exports.getProducts = (req, res, next) => {
         activeShop: true,
         productCSS: true
     });
-}
+};
+
+exports.get404 = (req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '' });
+};
