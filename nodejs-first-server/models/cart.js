d015a45eeb5201e36f28710942de98d5aebd97e4
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2022-07-04 19:46:57 -0400

    - connecting app sql

--- nodejs-first-server/models/cart.js
+++ nodejs-first-server/models/cart.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-  path.dirname(require.main.filename),
+  path.dirname(process.mainModule.filename),
   'data',
   'cart.json'
 );
--- nodejs-first-server/models/product.js
+++ nodejs-first-server/models/product.js
@@ -4,7 +4,7 @@ const path = require('path');
 const Cart = require('./cart');
 
 const p = path.join(
-  path.dirname(require.main.filename),
+  path.dirname(process.mainModule.filename),
   'data',
   'products.json'
 );
