a86d1e093658b1fd05ed88a3e057ad0f403ac1a7
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2022-07-07 08:21:49 -0400

    - mongodb connecting app to server

--- nodejs-first-server/util/database.js
+++ nodejs-first-server/util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://safayarma:miky101@cluster0.zlehf.mongodb.net/?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('Connected!');
+      callback(client);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
